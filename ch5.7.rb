62f701db3f815e4ed84da1948b3f490b578e5e36
David Fedele <dave.fedele@gmail.com>
2013-09-08 13:35:21 -0700

    :

---
+++ ch5.7.rb
@@ -0,0 +1,4 @@
+my_birth_month = 'August'
+my_birth_day = 3.to_s
+
+puts my_birth_month + my_birth_day
---
+++ ch6.rb
@@ -0,0 +1,17 @@
+# listing all methods covered
+
+# puts
+# gets
+# chomp
+# to_s
+# to_i
+# to_f
+# +
+# -
+# /
+# *
+
+puts ('hello'.+ 'world')
+puts ((10.* 9).+ 9)
+
+puts self
