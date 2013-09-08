f3e24aad0b76f583c17ab603e6f18b2bf2bf15f0
David Fedele <dave.fedele@gmail.com>
2013-09-08 13:21:52 -0700

    deleted files

--- favoriteNumber.rb
+++
@@ -1,4 +0,0 @@
-puts 'what is your favorite number?'
-favorite = gets.chomp.to_i
-new_favorite = favorite + 1
-puts 'I think ' + new_favorite.to_s + ' is a bigger and better favorite number!'
--- fullNameGreeting.rb
+++
@@ -1,7 +0,0 @@
-puts 'what is your first name?'
-first = gets.chomp
-puts 'what is your middle name?'
-middle = gets.chomp
-puts 'what is your last name?'
-last = gets.chomp
-puts 'very nice to meet you ' + first + ' ' + middle + ' ' + last + '!'
