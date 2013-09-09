a872475d6fdf4bf102ca2a995483b50e07989ec7
David Fedele <dave.fedele@gmail.com>
2013-09-09 00:11:17 -0700

    ch 6 added

---
+++ ch6.2.angryBoss.rb
@@ -0,0 +1,3 @@
+puts ('Why aren\'t you working?! What do u want!!?')
+question = gets.chomp
+puts ('ARE YOU KIDDING ME?! ' + question.upcase + '?! YOU\'RE FIRED!!')
---
+++ ch6.2.tableOfContents.rb
@@ -0,0 +1,11 @@
+line_width = 50
+puts('The Most Amazing Book In The World'.center(line_width))
+puts('Table of Contents'.center(line_width))
+
+line_width = 20
+print('Chapter 1: Introduction')
+puts('page 1'.rjust(line_width))
+print('Chapter 2: The End       ')
+puts('page 100'.rjust(line_width))
+print('Chapter 3: What Happened ')
+puts('page 923'.rjust(line_width))
