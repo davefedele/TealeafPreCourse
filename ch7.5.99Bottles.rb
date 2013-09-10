5f6501317e1f833cf770a2ed47922a5675c2065a
David Fedele <dave.fedele@gmail.com>
2013-09-10 00:34:26 -0700

    adjusting code for errors

--- ch7.5.99Bottles.rb
+++ ch7.5.99Bottles.rb
@@ -4,10 +4,10 @@ while bottles > 0
 	puts bottles.to_s + ' bottles of beer!'
 	puts 'You take one down.'
 	puts 'You Pass it around.'
-	if bottles  > 1
-		puts bottles.to_s + ' bottles of beer on the wall!'
+	if bottles  == 2
+		puts (bottles-1).to_s + ' bottle of beer on the wall!'
 	else
-		puts bottles.to_s + ' bottle of beer on the wall!'
+		puts (bottles-1).to_s + ' bottles of beer on the wall!'
 	end
 	puts
 	bottles = bottles - 1
--- ch7.5.leafYear.rb
+++ ch7.5.leafYear.rb
@@ -6,7 +6,9 @@ ending_year = gets.chomp
 year = start_year.to_i
 while year.to_i <= ending_year.to_i
 	if (year % 4) == 0
+		if year%100 != 0 || year%400 ==0
 		puts year.to_s + ' is a leap year'
+		end
 	end
 	year = year + 1
 end
