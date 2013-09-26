d6851ff94bbd3f788df6f2cb498937aa0a1f7cb4
David Fedele <dave.fedele@gmail.com>
2013-09-26 11:43:49 -0700

    ch 10 sort

---
+++ ch10.2.sort.rb
@@ -0,0 +1,28 @@
+def sort some_array
+  recursive_sort some_array, []
+end
+
+def recursive_sort unsorted_array, sorted_array
+  if unsorted_array.length <= 0
+    return sorted_array
+  end
+
+  smallest = unsorted_array.pop
+  not_sorted = []
+
+  unsorted_array.each do |word|
+    if word < smallest
+      not_sorted.push smallest
+      smallest = word
+    else
+      not_sorted.push word
+    end
+  end
+
+  sorted_array.push smallest
+
+  recursive_sort not_sorted, sorted_array
+end
+
+to_sort = ['boat','apple','bear']
+puts(sort(to_sort))
